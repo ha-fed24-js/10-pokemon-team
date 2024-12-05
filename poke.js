4ec120d35e6288a2ee66ad6e72680aa8d181c797
David Andersson <lejonmanen@gmail.com>
2024-12-05 15:56:20 +0100

    Flytta boxar till högra listan

--- poke.js
+++ poke.js
@@ -1,4 +1,6 @@
 const columns = document.querySelectorAll('.column')
+const chosenHeading = columns[1].querySelector('h2')
+const chosenTotalPower = columns[1].querySelector('.power')
 
 const pokemon = [
     { id: 1, name: 'Bulbasaur', cp: 439 },
@@ -9,6 +11,9 @@ const pokemon = [
     { id: 27, name: 'Sandshrew', cp: 710 },
     { id: 52, name: 'Meowth', cp: 121 }
 ]
+let totalCombatPower = 0
+const pokeColors = ['green', 'brown', 'yellow', 'pink']
+let selectedColor = 0
 
 initializePokemon()
 
@@ -22,9 +27,16 @@ function initializePokemon() {
 
 }
 function createPokemonBox(name, cp) {
+	const color = pokeColors[selectedColor]
+	selectedColor++
+	if( selectedColor >= pokeColors.length ) {
+		selectedColor = 0
+		// Detta kan göras smidigare med modulo-operatorn (%)
+	}
+
 	const section = document.createElement('section')
 	section.classList.add('pokemon')
-	section.innerHTML = `<div class="box"></div>`
+	section.innerHTML = `<div class="box ${color}"></div>`
 
 	const div = document.createElement('div')
 	div.classList.add('align-bottom')
@@ -36,5 +48,15 @@ function createPokemonBox(name, cp) {
 	div.append(h3, p)
 
 	section.append(div)
+
+	section.addEventListener('click', () => {
+		console.log('Du klickade på ' + name)
+		// columns[1].append( section )
+		chosenHeading.insertAdjacentElement('afterend', section)
+
+		// Uppdatera total combat power
+		totalCombatPower += cp
+		chosenTotalPower.innerText = `Total CP: ${totalCombatPower}.`
+	})
 	return section
 }
